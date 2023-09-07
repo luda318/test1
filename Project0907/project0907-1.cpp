e3474b83747ea4d62e3b03a227062f00cfa3f2e3
luda318 <144310681+luda318@users.noreply.github.com>
2023-09-07 16:08:57 +0900

    project0907-1

--- Project0907/project0907-1.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-	printf("Hello kmm!\n");
-	printf("bye\n");
-	return 0;
-}
---
+++ Project0907/project0907-2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+	{
+	int n;
+	int i, j;
+
+	scanf_s("%d", &n);
+	
+	for (i = 0; i<=n; i++) {
+		for (j = n; j >= i; j--) {
+			printf(" ");
+		}
+		for (j = 1; j <= 2*i -1; j++) {
+					printf("*");
+
+
+				}
+				printf("\n");
+				
+	}
+
+			
+						
+		}
+	return 0;
+	}
+	
+		
+
