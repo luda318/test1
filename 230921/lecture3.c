4f5d2609eb05841ffcd9965086778467b3a08c05
luda318 <144310681+luda318@users.noreply.github.com>
2023-09-21 15:22:50 +0900

    fact

---
+++ 230921/lecture3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/*int main(void) {
+
+	int i,j,res;
+	scanf_s("%d", &i);
+	for (j = 1; j = i; j++) {
+		res = j*;
+		printf("%d", res);
+		return 0;
+	}
+	return 0;
+}*/
+
+int fact(int num) {
+	if (num == 0) return 1;
+}
+
+int main() {
+	int num;
+	scanf_s("%d", &num);
+
+	printf("%d",num);
+	return 0;
+}
